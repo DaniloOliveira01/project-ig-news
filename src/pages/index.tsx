a1d48718cf43332d962b25582c59522439f7add8
Danilo Oliveira <oliveira.83555@gmail.com>
2022-02-13 22:53:49 -0300

    novo arquivo

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Página inicial | ig.news</title>
+      </Head>
+      <div>
+        <h1>Hello Word</h1>
+      </div>
+    </>
+  )
+}
